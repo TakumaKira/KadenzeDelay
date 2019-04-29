277bf20501edee17c6f74eb20ddde943d4157ddb
Takuma <mukudori09@gmail.com>
2019-04-29 13:51:05 +0900

    Fixed the sound problem which didn't occur in my case.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -141,9 +141,13 @@ void KadenzeDelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPe
         mCircularBufferLeft = new float[mCircularBufferLength];
     }
     
+    zeromem(mCircularBufferLeft, mCircularBufferLength * sizeof(float));
+    
     if (mCircularBufferRight == nullptr) {
         mCircularBufferRight = new float[mCircularBufferLength];
     }
+    
+    zeromem(mCircularBufferRight, mCircularBufferLength * sizeof(float));
     
     mCircularBufferWriteHead = 0;
     
